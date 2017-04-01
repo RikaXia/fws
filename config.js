6ed6606c7a29c61c103c2bf52a2318c94b0b6564
sbfkcel <sbfkcel@163.com>
2017-04-01 17:28:34 +0800

    添加模版文件

--- config.js
+++ config.js
@@ -1,4 +1,36 @@
-{
+//该配置会被项目目录下'fws_config.js'同键值所取代
+{   
+    //作者名称
     author:'name',
-    mail:'name@4399.com'
+    
+    //作者邮箱
+    mail:'name@4399.com',
+
+    //自动刷新
+    autoRefresh:true,
+    
+    //模版路径
+    tplPath:'',
+    
+    //字体路径
+    ttfPath:'',
+    
+    //源文件目录同步路径
+    srcSync:{
+        path:'',
+        fileType:'*'
+    },
+
+    //编译目录同步路径
+    devSync:{
+        path:'',
+        fileType:'*'
+    },
+
+    //发布目录同步路径
+    distSync:{
+        path:'',
+        fileType:'*'
+    }
+
 }
