5ec316a31401b63890a88d38ff39c78afe05f4c1
sbfkcel <sbfkcel@163.com>
2017-04-25 21:14:15 +0800

    监听jade文件

--- app/demo/src/data/index.js
+++
@@ -1,5 +0,0 @@
-let f = require('./public')
-module.exports = {
-    "title":f.header.title,
-    "footer":f.copyText
-};
--- task/watch.js
+++ task/watch.js
@@ -1,19 +1,24 @@
 const path = require('path');
 const chokidar = require('chokidar');
+const fs = require('fs');
 
 const tip = require('../lib/tip');                  //文字提示
-const isExist = require('../lib/isExist');          //判断文件或目录是否存在
+const pathInfo = require('../lib/getPathInfo');     //判断文件或目录是否存在
+const sass2css = require('../lib/sass2css');
 
 const cwdPath = process.cwd();                      //当前路径
 
+
+
+
+
+
+//let watch = ()=>{};
 class watch{
     constructor(projectPath){
         const _ts = this;
 
-        _ts.path = projectPath || cwdPath;
-        _ts.srcPath = path.join(_ts.path,'/src');   //src目录
-        _ts.devPath = path.join(_ts.path,'/dev');   //dev目录
-        _ts.distPath = path.join(_ts.path,'/dist'); //dist目录
+        _ts.path = projectPath || fws.cmdPath;
 
         _ts.init();
     }
@@ -35,7 +40,7 @@ class watch{
         const _ts = this;
         let path = _ts.path;
 
-        return isExist('src',path);
+        return pathInfo(fws.srcPath === 'dir');
     }
 
     //文件修改监听
@@ -47,8 +52,58 @@ class watch{
         
         tip.highlight(`已经启动项目监听，按"ctrl + c"取消监听变动`);
 
-        w.on('change',(path,stats)=>{
-            console.log(path);
+        w.on('change',(filePath,stats)=>{
+
+            let pt = pathInfo(filePath),
+                fileType = pt.extension,
+                fileName = pt.name;                
+
+            switch (fileType) {
+                case '.pug':
+                    //编译jade文件
+                    const pug = require('pug');
+
+                    let fn = pug.compileFile(filePath,{
+                        doctype:'html',
+                        pretty:true
+                    });
+
+
+                    
+                    let dataPath = path.join(fws.srcPath,'data','index.js');        //
+                    
+                    delete require.cache[dataPath];                                 //清除数据缓存
+
+                    let data = require(dataPath);
+
+                    let html = fn(data);
+
+                    console.log(data);
+                    console.log(html);
+
+
+
+                    
+                    
+                break;
+
+                case '.scss':
+                    //编译scss文件
+                    let dist = path.join(fws.devPath,'css',fileName+'.css');
+                    
+                    sass2css(filePath,dist,true);
+
+                break;
+
+                case '.ts':
+                    //编译typescript文件
+
+                break;
+            
+                // default:
+                //     tip.error(`${fileType}文件尚未指定编译任务。`)
+                // break;
+            }
         });
 
 
