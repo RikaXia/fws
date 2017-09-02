fb6272792400379b2c54907d9d9c501bcfcf6643
sbfkcel <sbfkcel@163.com>
2017-09-02 09:42:57 +0800

    _

--- tpl/data/_public.js
+++
@@ -1,4 +0,0 @@
-exports.copyText = "power by 4399.com &copy; 2017-2022";
-exports.header = {
-    "title":"页面标题"
-}
--- tpl/data/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-let fPath = path.join(__dirname,'_public');
-
-let f = fws.require(fPath);
-module.exports = {
-    "title":f.header.title,
-    "footer":f.copyText
-};
