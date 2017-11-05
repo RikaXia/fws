4da2b6854325aef9e68ccc3058ede666ed28d420
sbfkcel <sbfkcel@163.com>
2017-11-06 02:16:54 +0800

    调整Vue默认项目模版结构

--- tpl/vue/index.vue
+++ tpl/vue/index.vue
@@ -1,19 +1,32 @@
 <template lang="pug">
-  div.example {{ msg }}
+    div.example
+        div.main {{msg}}
+        v_footer
 </template>
 <script>
+import v_footer from '../public/footer.vue';
 export default {
-  data(){
-      return{
-          msg:'Hello'
-      }
-  }
+    components:{
+        v_footer
+    },
+    data(){
+        return {
+            msg:'Hello Vue'
+        }
+    }
 }
 </script>
 <style lang="scss">
-.example {
-    $color:red;
-    background:$color;
-}
+    .main {
+        $width:600px;
+        $height:80px;
+        $green:#42b983;
+        width:$width; height:$height; line-height:$height; color:$green; font-size:$height * 0.8; text-align:center; font-weight:bold; position:absolute; left:50%; top:40%;
+
+        margin:{
+            left:-($width / 2);
+            top:-($height / 2);
+        }
+    }
 </style>
 
