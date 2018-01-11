d94cee0100008227c62230b9ebf6db9ffce87bb3
fan <fan@fandeMacBook-Pro.local>
2018-01-11 17:33:16 +0800

    调整全局变量编译匹配

--- config.js
+++ config.js
@@ -35,6 +35,9 @@ module.exports = {
             {
                 find:'feutil.localstatic.com',
                 replace:'pic.my4399.com/re/cms/feUtil'
+            },{
+                find:'$$localhost/staticfile',
+                replace:'pic.my4399.com/re/cms/feUtil'
             }
         ]
     },
