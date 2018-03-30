98206d6df6fc6ab7a51cf742a4998b33ed9e003a
lisayang0562 <37896117+lisayang0562@users.noreply.github.com>
2018-03-30 15:57:03 +0800

    Update _public.js
    修改了UC、QQ浏览器下横竖屏

--- tpl/data/_public.js
+++ tpl/data/_public.js
@@ -1,7 +1,7 @@
 module.exports = {
     config:{
         isMobile:false,
-        isPortrait:false,
+        isPortrait:2,  // 0 代表竖屏  1 代表横屏  2 代表用户当前首选的方向
         isFullScreen:false,
         showIosIcon:false
     },
