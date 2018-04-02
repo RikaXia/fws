b6a898690344cc7d4699506f1b1cc64698fe78a0
fan <fan@fandeMacBook-Pro.local>
2018-04-02 09:40:22 +0800

    修复开启Server同步文件时，控制台引起的split报错的问题。更新 @二嫂 提交的模版修改意见

--- tpl/data/_public.js
+++ tpl/data/_public.js
@@ -1,9 +1,9 @@
 module.exports = {
     config:{
-        isMobile:false,
-        isPortrait:2,  // 0 代表竖屏  1 代表横屏  2 代表用户当前首选的方向
-        isFullScreen:false,
-        showIosIcon:false
+        isMobile:false,         // 是否为移动端页面
+        policy:'',              // portrait [默认] 纵向；landscape 横向；user 用户当前首选方向
+        isFullScreen:false,     // 是否为全屏
+        showIosIcon:false       // 是否适配IOS图标
     },
     title:'页面标题',
     copyText:'Power by fws'
